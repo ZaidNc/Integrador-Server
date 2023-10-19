19e62cf8a49d8139bd8d742d068ecd6e36871fa1
Daniel Navarro <danielkristopher0563@gmail.com>
2023-10-19 15:34:13 -0500

    waoss

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 const app = require("./app");
 const PORT = 3001;
+const { conn } = require('./DB_connection');
 
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+conn.sync({ force: true }).then(() => {
+  console.log('Database synced');
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
 });
